3357af4fef68ac19a3d7738dacc8f78ea2046c24
Vikneesh <65058468+Vikneesh52@users.noreply.github.com>
2023-07-06 01:18:03 +0530

    Update kmeans.h

--- kmeans.h
+++ kmeans.h
@@ -1,51 +1,179 @@
+#ifndef KMEANS_H
+#define KMEANS_H
+
 #include <stdio.h>
-#include "kmeans.h"
+#include <stdlib.h>
+#include <math.h>
 
-int main() {
-    // Sample data
-    double X[] = {1.0, 1.0, 2.0, 2.0, 5.0, 4.0, 5.0, 5.0, 9.0, 8.0, 9.0, 10.0};
-    int n_samples = 6;
-    int n_features = 2;
-    int n_clusters = 2;
+struct KMeansResult {
+    int* labels;
+    double* cluster_centers;
+    double inertia;
+};
 
-    // Fit K-means model
-    struct KMeansResult result = kmeans_fit(X, n_samples, n_features, n_clusters);
+struct KMeansResult kmeans_fit(double* X, int n_samples, int n_features, int n_clusters) {
+    struct KMeansResult result;
+    result.labels = (int*)malloc(n_samples * sizeof(int));
+    result.cluster_centers = (double*)malloc(n_clusters * n_features * sizeof(double));
+    result.inertia = 0.0;
 
-    // Print cluster labels
-    printf("Cluster labels:\n");
-    for (int i = 0; i < n_samples; i++) {
-        printf("%d ", result.labels[i]);
-    }
-    printf("\n");
-
-    // Print cluster centers
-    printf("Cluster centers:\n");
+    // K-means clustering algorithm implementation
+    // Initialize cluster centers randomly
     for (int i = 0; i < n_clusters; i++) {
         for (int j = 0; j < n_features; j++) {
-            printf("%.2f ", result.cluster_centers[i * n_features + j]);
+            result.cluster_centers[i * n_features + j] = X[rand() % (n_samples * n_features) + j];
         }
-        printf("\n");
     }
+
+    int max_iterations = 100;  // Maximum number of iterations
+    double tolerance = 1e-4;   // Convergence tolerance
+    double prev_inertia = INFINITY;
+
+    double inertia; // Declare inertia here
+
+    for (int iter = 0; iter < max_iterations; iter++) {
+        // Assign samples to the nearest cluster center
+        for (int i = 0; i < n_samples; i++) {
+            int closest_cluster = 0;
+            double min_distance = INFINITY;
+
+            for (int j = 0; j < n_clusters; j++) {
+                double distance = 0.0;
+                for (int k = 0; k < n_features; k++) {
+                    double diff = X[i * n_features + k] - result.cluster_centers[j * n_features + k];
+                    distance += diff * diff;
+                }
+
+                if (distance < min_distance) {
+                    min_distance = distance;
+                    closest_cluster = j;
+                }
+            }
+
+            result.labels[i] = closest_cluster;
+        }
+
+        // Update cluster centers
+        double* cluster_sums = (double*)calloc(n_clusters * n_features, sizeof(double));
+        int* cluster_counts = (int*)calloc(n_clusters, sizeof(int));
+
+        for (int i = 0; i < n_samples; i++) {
+            int cluster = result.labels[i];
+            cluster_counts[cluster]++;
+
+            for (int j = 0; j < n_features; j++) {
+                cluster_sums[cluster * n_features + j] += X[i * n_features + j];
+            }
+        }
+
+        for (int i = 0; i < n_clusters; i++) {
+            if (cluster_counts[i] > 0) {
+                for (int j = 0; j < n_features; j++) {
+                    result.cluster_centers[i * n_features + j] = cluster_sums[i * n_features + j] / cluster_counts[i];
+                }
+            }
+        }
+
+        free(cluster_sums);
+        free(cluster_counts);
+
+        // Compute the sum of squared distances to nearest cluster center (inertia)
+        inertia = 0.0;
+        for (int i = 0; i < n_samples; i++) {
+            int cluster = result.labels[i];
+            for (int j = 0; j < n_features; j++) {
+                double diff = X[i * n_features + j] - result.cluster_centers[cluster * n_features + j];
+                inertia += diff * diff;
+            }
+        }
+
+        // Check convergence
+        if (fabs(inertia - prev_inertia) < tolerance){            
+        break;
+        }
+        prev_inertia = inertia;
+    }
+
+    result.inertia = inertia;
+
+    return result;
+}
+
+
+int* kmeans_predict(double* X, int n_samples, int n_features, double* cluster_centers, int n_clusters) {
+    int* labels = (int*)malloc(n_samples * sizeof(int));
+
+    // Predict cluster labels based on the trained model
+    for (int i = 0; i < n_samples; i++) {
+        int closest_cluster = 0;
+        double min_distance = INFINITY;
+
+        for (int j = 0; j < n_clusters; j++) {
+            double distance = 0.0;
+            for (int k = 0; k < n_features; k++) {
+                double diff = X[i * n_features + k] - cluster_centers[j * n_features + k];
+                distance += diff * diff;
+            }
+
+            if (distance < min_distance) {
+                min_distance = distance;
+                closest_cluster = j;
+            }
+        }
+
+        labels[i] = closest_cluster;
+    }
+
+    return labels;
+}
+
+double* kmeans_transform(double* X, int n_samples, int n_features, double* cluster_centers, int n_clusters) {
+    double* distances = (double*)malloc(n_samples * n_clusters * sizeof(double));
 
     // Compute distances to cluster centers
-    double* distances = kmeans_transform(X, n_samples, n_features, result.cluster_centers, n_clusters);
-
-    // Print distances
-    printf("Distances to cluster centers:\n");
     for (int i = 0; i < n_samples; i++) {
         for (int j = 0; j < n_clusters; j++) {
-            printf("%.2f ", distances[i * n_clusters + j]);
+            double distance = 0.0;
+            for (int k = 0; k < n_features; k++) {
+                double diff = X[i * n_features + k] - cluster_centers[j * n_features + k];
+                distance += diff * diff;
+            }
+            distances[i * n_clusters + j] = sqrt(distance);
         }
-        printf("\n");
     }
 
-    // Compute inertia (sum of squared distances)
-    double inertia = kmeans_score(X, n_samples, n_features, result.cluster_centers, n_clusters);
-    printf("Inertia: %.2f\n", inertia);
-
-    // Free allocated memory
-    free_kmeans_result(result);
-    free(distances);
-
-    return 0;
+    return distances;
 }
+
+double kmeans_score(double* X, int n_samples, int n_features, double* cluster_centers, int n_clusters) {
+    double inertia = 0.0;
+
+    // Compute the sum of squared distances to nearest cluster center
+    for (int i = 0; i < n_samples; i++) {
+        int closest_cluster = 0;
+        double min_distance = INFINITY;
+
+        for (int j = 0; j < n_clusters; j++) {
+            double distance = 0.0;
+            for (int k = 0; k < n_features; k++) {
+                double diff = X[i * n_features + k] - cluster_centers[j * n_features + k];
+                distance += diff * diff;
+            }
+
+            if (distance < min_distance) {
+                min_distance = distance;
+            }
+        }
+
+        inertia += min_distance;
+    }
+
+    return inertia;
+}
+
+void free_kmeans_result(struct KMeansResult result) {
+    free(result.labels);
+    free(result.cluster_centers);
+}
+
+#endif  // KMEANS_H
